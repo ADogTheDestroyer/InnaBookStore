c1bf9d07cb3eb79268890a67a80913ce35fa4bfc
Cole-Kaufman-1 <cole.kaufman1@gmail.com>
2022-12-09 00:00:04 -0500

    Changed drop all tables query, added most mock data, still need to add orders, In_orders and Order_addrs

--- DropAllTablesQuery.sql
+++ DropAllTablesQuery.sql
@@ -1,5 +1,22 @@
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO public;
-COMMENT ON SCHEMA public IS 'standard public schema';
+drop table phone_numbers cascade;
+
+drop table user_addrs cascade;
+
+drop table order_addrs cascade;
+
+drop table orders cascade;
+
+drop table users cascade;
+
+drop table genres cascade;
+
+drop table authored cascade;
+
+drop table authors cascade;
+
+drop table books cascade;
+
+drop table publishers cascade;
+
+drop table addresses cascade;
+
