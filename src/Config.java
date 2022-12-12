2ea376fac33affbe08789cace58db27e981ea0d3
Cole Kaufman <49255548+Cole-Kaufman-1@users.noreply.github.com>
2022-12-11 23:19:24 -0500

    Update Config.java

--- src/Config.java
+++ src/Config.java
@@ -1,5 +1,5 @@
 public interface Config {
-    String connectionUrl = "jdbc:postgresql://localhost:5432/LookInnaBookDb";
+    String connectionUrl = "jdbc:postgresql://localhost:5432/LookInnaBook";
     String username = "postgres";
     String password = "password";
 }
